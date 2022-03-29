afcda10b806be839b996ae278beaeed64ca8fa78
Carlos0ML <carlosmonerrislupion@gmail.com>
2022-03-29 16:11:37 +0200

    Finalización de la API y Documentación útil

--- api.py
+++ api.py
@@ -2,23 +2,26 @@ from flask import *
 import sqlite3
 import json
 
-# Realización de los imports, en esta APIres haremos uso del framework "Flask", gestionaremos la base de datos con "Sqlite3", y por último, realizaremos la importación de los archivos JSON con su respectivo módulo.
+'''Realización de los imports, en esta APIres haremos uso del framework
+"Flask", gestionaremos la base de datos con "Sqlite3", y por último,
+realizaremos la importación de los archivos JSON con su respectivo módulo.'''
 
 con = sqlite3.connect('database.db', check_same_thread=False)
-cur = con.cursor()							# Conexión con la BD, el cursor y FLask
+cur = con.cursor()							'''Conexión con la BD, el cursor y FLask.'''
 app = Flask(__name__)
 
-# La base de datos utlizada es sobre arquitectos y edificios.
+
+'''La base de datos utlizada es sobre arquitectos y edificios.'''
 
 
-def devolver_atrib_arquitecto():	# Devolución de atributos de tabla "arquitecto".
+def devolver_atrib_arquitecto():	'''Devolución de atributos de tabla "arquitecto".'''
     return [
         'arquitecto',
         'num_colegio'
     ]
 
 
-def devolver_atrib_edificio():	# Devolución de atributos de la tabla "edificio".
+def devolver_atrib_edificio():	'''Devolución de atributos de la tabla "edificio".'''
     return [
         'id',
         'denominacion',
@@ -43,8 +46,12 @@ def devolver_atrib_edificio():	# Devolución de atributos de la tabla "edificio"
     ]
 
 
-def mapear_registros(atributos_tabla, cursor):	# Esta función nos permitirá mapear, o mostrar en la web la información de los
-    for fila in cursor:				# registros.
+def mapear_registros(atributos_tabla, buffer_temporal):
+			
+'''Esta función nos permitirá mapear, o mostrar
+   en la web la información de los registros.'''
+
+    for fila in buffer_temporal:				
         registro = {}
         i = 0
         while i < len(fila):
@@ -57,9 +64,13 @@ def mapear_registros(atributos_tabla, cursor):	# Esta función nos permitirá ma
 
 
 
-@app.route("/")	# Definición de la ruta raíz, o path.
-def index():		# Esta función va a mostrar mediante la sentencia SELECT y con la asistencia de las funciones de devolución de 
-    atributos_arquitecto = devolver_atrib_arquitecto()	# atributos, los datos de arquitecto y edificio.
+@app.route("/")	''' Definición de la ruta raíz, o path.'''
+def index():	
+
+'''Esta función va a mostrar mediante la sentencia SELECT y con laasistencia de
+las funciones de devolución de atributos, los datos de arquitecto y edificio.'''
+
+    atributos_arquitecto = devolver_atrib_arquitecto()	
     atributos_edificio = devolver_atrib_edificio()
 
     cur.execute("SELECT * FROM arquitecto")
@@ -68,21 +79,30 @@ def index():		# Esta función va a mostrar mediante la sentencia SELECT y con la
     cur.execute("SELECT * FROM edificio")
     edificios = mapear_registros(atributos_edificio, cur)
     
-    return Response(json.JSONEncoder().encode([arquitectos, edificios]), mimetype='application/json')	# Finalmente, nos devolverá
-														# un JSON con la información
+    return Response(json.JSONEncoder().encode([arquitectos, edificios]), mimetype='application/json')	
+    
+    '''Finalmente, nos devolverá un JSON con la información'''
+    
+@app.route("/Edificio")	'''Definición de la ruta Edificio, la cual va a contener toda la información de la tabla edificio.'''
+def index_edificio():
 
-@app.route("/Edificio")	# Definición de la ruta Edificio, la cual va a contener toda la información de la tabla edificio.
-def index_edificio():		# Función que nos mostrará los datos e información de edificio, usando la sentencia SELECT y las funciones 
-    atributos_edificio = devolver_atrib_edificio()	# de devolución de atributos.
+'''Función que nos mostrará los datos e información de edificio,
+usando la sentencia SELECT y las funciones de devolución de atributos.'''
+
+    atributos_edificio = devolver_atrib_edificio()
     cur.execute("SELECT * FROM edificio")
     edificios = mapear_registros(atributos_edificio, cur)
     
     return Response(json.JSONEncoder().encode(edificios), mimetype='application/json')
 
 
-@app.route("/Arquitecto")	# Definición de la ruta Arquitecto, contendrá toda la información de la tabla arquitecto.
-def index_arquitecto():	# Función que nos mostrará los datos e información de edificio, usando la sentencia SELECT y las funciones
-    atributos_arquitecto = devolver_atrib_arquitecto()	# de devolución de atributos.
+@app.route("/Arquitecto")	'''Definición de la ruta Arquitecto, contendrá toda la información de la tabla arquitecto.'''
+def index_arquitecto():	
+
+'''Función que nos mostrará los datos e información de edificio,
+usando la sentencia SELECT y las funciones de devolución de atributos.'''
+
+    atributos_arquitecto = devolver_atrib_arquitecto()
 
     cur.execute("SELECT * FROM arquitecto")
     arquitectos = mapear_registros(atributos_arquitecto, cur)
@@ -90,12 +110,17 @@ def index_arquitecto():	# Función que nos mostrará los datos e información de
     return Response(json.JSONEncoder().encode(arquitectos), mimetype='application/json')
 
 
-@app.route("/Edificio/eliminar/<id>")		# Definición de la ruta Eliminar Edificio, en esta ruta se va a eliminar por fila los 
-def eliminar_edificio(id):			# edificios.
+@app.route("/Edificio/eliminar/<id>")
+
+'''Definición de la ruta Eliminar Edificio, en
+esta ruta se va a eliminar por fila los edificios.'''
+
+def eliminar_edificio(id):			
     
     parametros = (id,)
-    sql = "DELETE FROM edificio WHERE id = ?;"	# Se hará uso de la sentencia DELETE para eliminar edificios por id.
-    							# Por último, se ofrecerá un feedback mínimo mediante el uso de excepciones
+    sql = "DELETE FROM edificio WHERE id = ?;"	'''Se hará uso de la sentencia DELETE para eliminar edificios por id.'''
+    
+    							'''Por último, se ofrecerá un feedback mínimo mediante el uso de excepciones.'''
     try:
         cur.execute(sql, parametros)
         con.commit()
@@ -105,25 +130,37 @@ def eliminar_edificio(id):			# edificios.
     return redirect(url_for('index'))
 
 
-@app.route("/Arquitecto/eliminar/<arquitecto>")	# Definición de la ruta Eliminar Arquitecto, en esta ruta se va a eliminar por
-def eliminar_arquitecto(arquitecto):			# fila los arquitectos.
+@app.route("/Arquitecto/eliminar/<arquitecto>")	
+
+'''Definición de la ruta Eliminar Arquitecto, en
+esta ruta se va a eliminar por fila los arquitectos.'''
+
+def eliminar_arquitecto(arquitecto):			
 
     parametros = (arquitecto,)
-    sql = "DELETE FROM arquitecto WHERE arquitecto = ?;"	# Al igual que la anterior función, esta se centrará en eliminar filas, en
-								# este caso de arquitectos.
+    sql = "DELETE FROM arquitecto WHERE arquitecto = ?;"
+    	
+    '''Al igual que la anterior función, esta se centrará
+    en eliminar filas, en este caso de arquitectos.'''
+    
     try:
         cur.execute(sql, parametros)
         con.commit()
     except:
         return 'No se ha podido eliminar el registro'
 
-    return redirect(url_for('index'))				# Finalmente, nos redirigirá al index.
+    return redirect(url_for('index'))				'''Finalmente, nos redirigirá al index.'''
 
 
-@app.route('/Edificio/buscar/<denominacion>')		# Definición de la ruta Buscar Edificio, esta ruta nos permite realizar la búsqueda
-def buscar_edificio(denominacion):			# de un edificio.
-    '''Esta función permite realizar la búsqueda de un registro
-    mediante el nombre del proyecto'''
+@app.route('/Edificio/buscar/<denominacion>')
+
+'''Definición de la ruta Buscar Edificio, esta ruta 
+os permite realizar la búsqueda de un edificio.'''
+
+def buscar_edificio(denominacion):
+
+    '''Esta función permite realizar la búsqueda de
+    un registro mediante el nombre del proyecto'''
 
     atributos_edificio = devolver_atrib_edificio()
     
@@ -138,10 +175,15 @@ def buscar_edificio(denominacion):			# de un edificio.
     return Response(json.JSONEncoder().encode(busqueda), mimetype='application/json')
 
 
-@app.route('/Arquitecto/buscar/<arquitecto>')	# Definición de la ruta Buscar Arquitecto, esta ruta nos permite realizar la búsqueda de 
-def buscar_arquitecto(arquitecto):		# arquitectos.
-    '''Esta función permite realizar la búsqueda de un registro
-    mediante el nombre del proyecto'''
+@app.route('/Arquitecto/buscar/<arquitecto>')	
+	
+'''Definición de la ruta Buscar Arquitecto, esta ruta nos
+permite realizar la búsqueda de arquitectos.'''
+
+def buscar_arquitecto(arquitecto):
+
+    '''Esta función permite realizar la búsqueda de
+    un registro mediante el nombre del proyecto'''
 
     atributos_arquitecto = devolver_atrib_arquitecto()
     
@@ -156,13 +198,21 @@ def buscar_arquitecto(arquitecto):		# arquitectos.
     return Response(json.JSONEncoder().encode(busqueda), mimetype='application/json')
 
 
-@app.route('/Edificio/modificar', methods=['GET', 'POST']) 	# Definición de la ruta Modificar Edificio, esta ruta nos permite modificar 
-def modificar_edificio():					# los valores de los atributos de una fila de edificio.
+@app.route('/Edificio/modificar', methods=['GET', 'POST']) 	
+
+'''Definición de la ruta Modificar Edificio, esta ruta nos permite modificar.'''
+
+def modificar_edificio():						'''Los valores de los atributos de una fila de edificio.'''
     con = sqlite3.connect('database.db', check_same_thread=False)
     
-    if request.method == 'GET':				# Nos apoyaremos en los verbos del protocolo http, usando GET y POST. GET
-        atributos_edificio = devolver_atrib_edificio()	# nos añadirá los datos codificados a la URL y POST añadirá los datos al
-        try:							# cuerpo, y no a la URL.
+    if request.method == 'GET':
+    
+    '''Nos apoyaremos en los verbos del protocolo http, usando GET y POST.
+    GET nos añadirá los datos codificados a la URL y POST añadirá los datos
+    al cuerpo, y no a la URL.'''
+    
+        atributos_edificio = devolver_atrib_edificio()
+        try:
             cur.execute('''SELECT * FROM edificio WHERE id={}'''.format(request.args.get('id')))
         except:
             return 'No se ha podido realizar la búsqueda'
@@ -177,9 +227,13 @@ def modificar_edificio():					# los valores de los atributos de una fila de edif
 
         for atributo, nuevo_valor in data.items():
             parametros = (nuevo_valor, data['id'])
-            sql = '''UPDATE edificio set {} = ? WHERE id = ?'''.format(atributo)	# Cuando se ejecute la sentencia UPDATE, se 
-            try:									# actualizarán los datos de la BD y nos va a 
-                cur.execute(sql, parametros)						# redirigir al index.
+            sql = '''UPDATE edificio set {} = ? WHERE id = ?'''.format(atributo)	
+            
+            '''Cuando se ejecute la sentencia UPDATE, se actualizarán
+            los datos de la BD y nos va a redirigir al index.'''
+            
+            try:
+                cur.execute(sql, parametros)
             except:
                 return 'No se ha podido actualizar el registro'
             
@@ -188,46 +242,71 @@ def modificar_edificio():					# los valores de los atributos de una fila de edif
         return redirect(url_for('index'))    
 
 
-@app.route('/Arquitecto/modificar', methods=['GET', 'POST'])	# Definición de la ruta Modificar Arquitecto, la cual nos permitirá
-def modificar_arquitecto():					# modificar los datos de las filas de la tabla arquitecto.
+@app.route('/Arquitecto/modificar', methods=['GET', 'POST'])
+
+'''Definición de la ruta Modificar Arquitecto, la cual nos
+permitirá modificar los datos de las filas de la tabla arquitecto.'''
+
     con = sqlite3.connect('database.db', check_same_thread=False)
     
-    if request.method == 'GET':	# Nos va a hacer visible en la web los datos que modifiquemos.
+    if request.method == 'GET':
+    
+    '''Nos va a hacer visible en la web los datos que modifiquemos.'''
+    
         atributos_arquitecto = devolver_atrib_arquitecto()
         try:
             cur.execute('''SELECT * FROM arquitecto WHERE arquitecto={}'''.format(request.args.get('arquitecto')))
-        except:						# Después de ejecutar la sentencia SELECT, nos va a mostrar los datos de
-            return 'No se ha podido realizar la búsqueda'	# la tabla arquitectos en la BD.
+        except:
+            return 'No se ha podido realizar la búsqueda'	
+            
+            '''Después de ejecutar la sentencia SELECT, nos va
+            a mostrar los datos de la tabla arquitectos en la BD.'''
 
         busqueda = mapear_registros(atributos_arquitecto, cur)
         con.commit()
 
-        return Response(json.JSONEncoder().encode(busqueda), mimetype='application/json')	# Nos devolverá una vista del JSON con los 
-												# valores actualizados.
-    elif request.method == 'POST':	  # Nos va a modificar los valores en el cuerpo
-        data = json.loads(request.data) # pero no en la url.
+        return Response(json.JSONEncoder().encode(busqueda), mimetype='application/json')
+        
+        '''Nos devolverá una vista del JSON con los valores actualizados.'''
+												
+    elif request.method == 'POST':
+        data = json.loads(request.data)
+        
+         '''Nos va a modificar los valores en el cuerpo pero no en la URL.'''
 
         for atributo, nuevo_valor in data.items():
             parametros = (nuevo_valor, data['arquitecto'])
-            sql = '''UPDATE arquitecto set {} = ? WHERE arquitecto = ?'''.format(atributo)	# Después de ejecutar la sentencia UPDATE,
-            try:										# nos va a actualizar los datos de la tabla
-                cur.execute(sql, parametros)							# arquitecto en la BD y nos va a redirigir 
-            except:										# al index.
+            sql = '''UPDATE arquitecto set {} = ? WHERE arquitecto = ?'''.format(atributo)	
+            
+            '''Después de ejecutar la sentencia UPDATE, nos va a actualizar los
+            datos de la tabla arquitecto en la BD y nos va a redirigir al index.'''
+            
+            try:							
+                cur.execute(sql, parametros)
+            except:
                 return 'No se ha podido actualizar el registro'
             
         con.commit()
         
         return redirect(url_for('index'))
 
-@app.route('/Arquitecto/anadir', methods=['POST'])	# Definición de la ruta Añadir Arquitecto, la cual nos permitirá realizar adiciones
-def anadir_arquitecto():				# de filas de la tabla arquitecto.
+@app.route('/Arquitecto/anadir', methods=['POST'])
+
+'''Definición de la ruta Añadir Arquitecto, la cual
+nos permitirá realizar adiciones de filas de la tabla
+arquitecto.'''
+
+def anadir_arquitecto():				
     data = json.loads(request.data)
     atributos_arquitecto = devolver_atrib_arquitecto()
     
     parametros = (data['arquitecto'], data['num_colegio'])
     sql = """ INSERT INTO arquitecto (arquitecto, num_colegio) 	
-                          VALUES (?, ?) """				# Ejectuaremos la setencia INSERT para darle valor a los atributos
-									# de arquitecto.
+                          VALUES (?, ?) """				
+                          
+                          '''Ejectuaremos la setencia INSERT para darle
+                          valor alos atributos de arquitecto.'''
+
     try:
         cur.execute(sql, parametros)
     except:
@@ -235,11 +314,15 @@ def anadir_arquitecto():				# de filas de la tabla arquitecto.
             
     con.commit()
         
-    return redirect(url_for('index'))					# Se nos redirigirá al index.
+    return redirect(url_for('index'))		'''Se nos redirigirá al index.'''
 
         
-@app.route('/Edificio/anadir', methods=['POST'])	# Definición de la ruta Añadir Edificio, la cual nos permitirá realizar adiciones
-def anadir_edificio():					# por fila a la tabla edificio.
+@app.route('/Edificio/anadir', methods=['POST'])
+
+'''Definición de la ruta Añadir Edificio, la cual nos
+permitirá realizar adiciones por fila a la tabla edificio.'''
+
+def anadir_edificio():
     data = json.loads(request.data)
     atributos_edificio = devolver_atrib_edificio()
     
@@ -249,8 +332,11 @@ def anadir_edificio():					# por fila a la tabla edificio.
         ubicacionActual, acceso, formaDeIngreso, procedencia, volumen,
         cronologia, autores, descripcion, historia, objetoDocumento, tecnicas,
         signatura, exposicion, thumbnail) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) """	# Se ejecutará la sentencia INSERT para añadir a los
-											# valores a los atributos de la tabla edificio.
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) """	
+        
+        '''Se ejecutará la sentencia INSERT para añadir a los
+        valores a los atributos de la tabla edificio.'''
+        
     try:
         cur.execute(sql, parametros)
     except:
@@ -258,4 +344,4 @@ def anadir_edificio():					# por fila a la tabla edificio.
             
     con.commit()
         
-    return redirect(url_for('index'))		# Se nos redirigirá al index.
+    return redirect(url_for('index'))		'''Se nos redirigirá al index.'''
